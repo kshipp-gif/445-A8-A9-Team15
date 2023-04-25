e46022cdc346f337c4d55c7d3ba6e8a6787a0590
Jose MHS <joseherrera121301@yahoo.com>
2023-04-24 23:37:42 -0700

    updated staff stuff n removed old staff

--- 445-A8-A9-Team15/Staff.aspx.cs
+++ 445-A8-A9-Team15/Staff.aspx.cs
@@ -8,50 +8,64 @@ using System.IO;
 using System.Xml;
 using System.Xml.Linq;
 using JoseDllLibrary;
+using System.Data;
 
 namespace JoseRequiredServicesTryItPage {
     public partial class WebForm2 : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-            XDocument xmlDoc = XDocument.Load(Server.MapPath("~/App_Data/Staff.xml"));
-            // Extract the data you want into a list
-            var data = xmlDoc.Descendants("Member")
-                             .Select(m => new {
-                                 Username = (string)m.Attribute("username"),
-                                 Password = (string)m.Attribute("password")
-                             })
-                             .ToList();
+
+            DataSet ds = new DataSet();
+            ds.ReadXml(Server.MapPath("~/App_Data/Staff.xml"));
 
             // Bind the list to the GridView
-            GridView1.DataSource = data;
+            GridView1.DataSource = ds;
             GridView1.DataBind();
         }
 
         protected void Unnamed5_Click(object sender, EventArgs e) {
             // this is where we add member to staff
+            EncryptionAndDecrypion Encryptor = new EncryptionAndDecrypion();
             string memberName = usernameInput.Text;
             string memberPassword = passwordInput.Text;
+
+            string encryptedPassword = Encryptor.encrypt(memberPassword);
             // Load the XML file
-            XDocument xmlDoc = XDocument.Load(Server.MapPath("~/App_Data/Staff.xml"));
+            string filepath = HttpRuntime.AppDomainAppPath + @"\App_Data\Staff.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filepath);
+            XmlElement rootElement = xmlDoc.DocumentElement; // grab XML root node
+            XmlNodeList nodeList = xmlDoc.SelectNodes("members/member");
 
-            // Add the new Member element
-            xmlDoc.Element("Members").Add(new XElement("Member",
-                new XAttribute("username", memberName),
-                new XAttribute("password", memberPassword)));
+            foreach (XmlNode staffNode in nodeList) {
+                foreach (XmlNode inNode in staffNode.ChildNodes) {
+                    if(inNode.InnerText == memberName) {
+                        // already exists
+                        return;
+                    }
+                }
+            }
 
+            XmlElement newStaff = xmlDoc.CreateElement("member", rootElement.NamespaceURI);
+            rootElement.AppendChild(newStaff);
+
+            XmlElement newStaffName = xmlDoc.CreateElement("username", rootElement.NamespaceURI);
+            newStaff.AppendChild(newStaffName);
+            newStaffName.InnerText = memberName;
+
+            XmlElement newStaffPassword = xmlDoc.CreateElement("password", rootElement.NamespaceURI);
+            newStaff.AppendChild(newStaffPassword);
+            newStaffPassword.InnerText = encryptedPassword;
+            
             // Save the updated XML file
-            xmlDoc.Save(Server.MapPath("~/App_Data/Testing.xml"));
+            xmlDoc.Save(Server.MapPath("~/App_Data/Staff.xml"));
 
             // Re-bind the GridView to the updated XML file
             // Extract the data you want into a list
-            var data = xmlDoc.Descendants("Member")
-                             .Select(m => new {
-                                 Username = (string)m.Attribute("username"),
-                                 Password = (string)m.Attribute("password")
-                             })
-                             .ToList();
+            DataSet ds = new DataSet();
+            ds.ReadXml(Server.MapPath("~/App_Data/Staff.xml"));
 
             // Bind the list to the GridView
-            GridView1.DataSource = data;
+            GridView1.DataSource = ds;
             GridView1.DataBind();
         }
     }
